7353abfa7ff0f60429152e0fc2b4a21cbeee9a81
Cristian <cris18002011@hotmail.com>
2022-05-02 10:58:12 -0500

    Paso 9: Modularizacion servicio FizzBuzz

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 //import reader from "./libs/utils/reader.js";
-const explorerService = require('./libs/services/explorerService.js')
 const reader = require('./libs/utils/reader.js')
+const explorerService = require('./libs/services/explorerService.js')
+const fizzBuzzService = require('./libs/services/fizzBuzzService')
 
 const explorers = reader.readJsonFile("explorers.json") // esto regresa la lista de explorers del archivo
 //console.log(explorers)
@@ -11,7 +12,23 @@ const expByMss = explorerService.filterByMission(explorers, 'node')
 const countExpByMss = explorerService.getAmountOfExplorersByMission(explorers, 'node')
 const userByMss = explorerService.getExplorersUsernamesByMission(explorers,'node')
 
-console.log(expByMss)
-console.log(countExpByMss)
-console.log(userByMss)
+// console.log(expByMss)
+// console.log(countExpByMss)
+// console.log(userByMss)
 
+const explorer1 = {name: "Explorer1", score: 1}
+fizzBuzzService.applyValidationInExplorer(explorer1) // {name: "Explorer1", score: 1, trick: 1} 
+
+const explorer3 = {name: "Explorer3", score: 3}
+fizzBuzzService.applyValidationInExplorer(explorer3) // {name: "Explorer3", score: 3, trick: "FIZZ"}
+
+const explorer5 = {name: "Explorer5", score: 5}
+fizzBuzzService.applyValidationInExplorer(explorer5) // {name: "Explorer5", score: 5, trick: "BUZZ"}
+
+const explorer15 = {name: "Explorer15", score: 15}
+fizzBuzzService.applyValidationInExplorer(explorer15) // {name: "Explorer15", score: 15, trick: "FIZZBUZZ"}
+
+console.log(explorer1)
+console.log(explorer3)
+console.log(explorer5)
+console.log(explorer15)
