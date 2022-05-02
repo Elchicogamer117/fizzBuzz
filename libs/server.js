6f943a4085a326c2c3a159c359026421f9c1c729
Cristian <cris18002011@hotmail.com>
2022-05-02 17:17:03 -0500

    Paso 18: Agregando y probando endpoints

--- libs/server.js
+++ libs/server.js
@@ -8,13 +8,25 @@ app.use(express.json())
 app.get('/',(request, response) => {
       response.json({message: "FizzBuzz API welcome 🖖!"})
 });
-
-app.get('/v1/explorers/:mission',(request, response) => { 
+//*endpoint 1
+app.get('/v1/explorers/:mission',(request, response) => { //*? :mission query param
       const mission = request.params.mission
       const explorerInMission = explorerController.getExplorersByMission(mission)
       response.json(explorerInMission)
 });
-
+//*endpoint 2
+app.get('/v1/explorers/amount/:mission',(request, response) => {
+      const mission = request.params.mission
+      const explorerInMission = explorerController.getExplorersAmonutByMission(mission)
+      response.json(`mission: ${mission}, quantity: ${explorerInMission}`)
+});
+//*endpoint 3
+app.get('/v1/explorers/usernames/:mission',(request, response) => {
+      const mission = request.params.mission
+      const explorerInMission = explorerController.getExplrersUsernamesByMission(mission)
+      response.json(`mission: ${mission}, explorers: ${explorerInMission}`)
+});
+//*Starting
 app.listen(port, () => {
       console.log(`FizzBuzz API in localhost: ${port}`)
 });
