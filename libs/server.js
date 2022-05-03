a9079c24e469c8e4a1caba9abeb2166831a2e8c0
Cristian <cris18002011@hotmail.com>
2022-05-02 22:00:04 -0500

    Paso 20: Agregando y probando nuevo requerimiento (endpoint 4)

--- libs/server.js
+++ libs/server.js
@@ -1,32 +1,44 @@
-const explorerController = require('./controllers/explorerController');
-const express = require('express');
+const explorerController = require("./controllers/explorerController");
+const express = require("express");
 const app = express();
 const port = 3000;
 
-app.use(express.json())
+app.use(express.json());
 
-app.get('/',(request, response) => {
-      response.json({message: "FizzBuzz API welcome 🖖!"})
+app.get("/",(request, response) => {
+	response.json({message: "FizzBuzz API welcome 🖖!"});
 });
 //*endpoint 1
-app.get('/v1/explorers/:mission',(request, response) => { //*? :mission query param
-      const mission = request.params.mission
-      const explorerInMission = explorerController.getExplorersByMission(mission)
-      response.json(explorerInMission)
+app.get("/v1/explorers/:mission",(request, response) => { //*? :mission query param
+	const mission = request.params.mission;
+	const explorerInMission = explorerController.getExplorersByMission(mission);
+	response.json(explorerInMission);
 });
 //*endpoint 2
-app.get('/v1/explorers/amount/:mission',(request, response) => {
-      const mission = request.params.mission
-      const explorerInMission = explorerController.getExplorersAmonutByMission(mission)
-      response.json(`mission: ${mission}, quantity: ${explorerInMission}`)
+app.get("/v1/explorers/amount/:mission",(request, response) => {
+	const mission = request.params.mission;
+	const explorerInMission = explorerController.getExplorersAmonutByMission(mission);
+	response.json({mission: mission, quantity: explorerInMission });
 });
 //*endpoint 3
-app.get('/v1/explorers/usernames/:mission',(request, response) => {
-      const mission = request.params.mission
-      const explorerInMission = explorerController.getExplrersUsernamesByMission(mission)
-      response.json(`mission: ${mission}, explorers: ${explorerInMission}`)
+app.get("/v1/explorers/usernames/:mission",(request, response) => {
+	const mission = request.params.mission;
+	const explorerInMission = explorerController.getExplrersUsernamesByMission(mission);
+	response.json({mission: mission, explorers: explorerInMission});
+});
+//*endpoint request 4
+app.get("/v1/explorers/fizzbuzz/:score",(request, response) => {
+	const number = request.params.score;
+	const numberRequest = explorerController.getValidationInNumber(number);
+	response.json({score: number, trick: numberRequest});
 });
 //*Starting
-app.listen(port, () => {
-      console.log(`FizzBuzz API in localhost: ${port}`)
+let server = app.listen(port, () => {
+	console.log(`FizzBuzz API in localhost: ${port}`);
 });
+
+let finish = () => {
+	server.close();
+};
+
+module.exports = {app, finish};
